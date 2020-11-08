ccadb4939e71bacc7ec31fcdb8351aae574bc01f
Cooper Tingey <cooper.tingey@gmail.com>
2020-11-07 18:04:18 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+# casino slot machine
+
+# 4 slots, 4 options (7, cherries, bomb, diamonds,)
+
+#cost: 3$, winnings if:
+# contains anybomb: lose 3$
+# +$2 for every 7
+# +4$ for every cherry
+# +$10 for every diamond
+# +$100 all of one
+
+options = {
+  1: 'bomb',
+  2: '777',
+  3: 'cherries',
+  4: 'diamonds'
+}
