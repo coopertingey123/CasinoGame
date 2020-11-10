797f5b0acce2a54dbd023230129dd206b9867f73
Cooper Tingey <cooper.tingey@gmail.com>
2020-11-09 23:30:03 -0700

    random array

--- app.py
+++ app.py
@@ -18,5 +18,30 @@ options = {
 
 wallet = 100
 
+# I need to create a container that holds all 256 possible combinations of the slot machine
+# From there, I need to pick one out of the 256, so that that combination cannot be  used again until it all starts over and the 256 refreshes
+# Do the math to make the house always profit from those 256 results
+
+import random
 
 
+def slot_machine():
+
+  mini_container = []
+  options_list = [1,2,3,4]
+  for num in options_list:
+    choice = random.choice(options_list)
+    mini_container.append(choice)
+  return mini_container
+  
+      
+
+slot_options = {
+  1: {'bomb': 'Better luck next time!'},
+  2: {'777': 'Add $2'},
+  3: {'cherries': 'Add $4'},
+  4: {'diamonds': 'Add $12'}
+}
+
+
+print(slot_machine())
