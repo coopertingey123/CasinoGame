49bba4a4a28245ec957ed04c3b2bbd50139a7794
Cooper Tingey <cooper.tingey@gmail.com>
2020-11-10 23:36:37 -0700

    Updated randomizer

--- app.py
+++ app.py
@@ -8,16 +8,6 @@
 # +4$ for every cherry
 # +$10 for every diamond
 # +$100 all of one
-
-options = {
-  1: {'bomb': 'Better luck next time!'},
-  2: {'777': 'Add $2'},
-  3: {'cherries': 'Add $4'},
-  4: {'diamonds': 'Add $12'}
-}
-
-wallet = 100
-
 # I need to create a container that holds all 256 possible combinations of the slot machine
 # From there, I need to pick one out of the 256, so that that combination cannot be  used again until it all starts over and the 256 refreshes
 # Do the math to make the house always profit from those 256 results
@@ -25,16 +15,10 @@ wallet = 100
 import random
 
 
-def slot_machine():
 
-  mini_container = []
-  options_list = [1,2,3,4]
-  for num in options_list:
-    choice = random.choice(options_list)
-    mini_container.append(choice)
-  return mini_container
-  
-      
+
+       
+                    
 
 slot_options = {
   1: {'bomb': 'Better luck next time!'},
@@ -45,3 +29,45 @@ slot_options = {
 
 
 print(slot_machine())
+
+import random
+
+winnings = 0
+main_container = []
+result = []
+wallet = 100
+def slot_machine():
+    winnings = 0
+    for i in range(4): 
+        for j in range(4): 
+            for k in range(4):
+              for l in range(4):
+                  
+                 main_container.append([i, j, k, l])
+    random_selection = random.choice(main_container)
+    for num in random_selection:
+      if num == 0:
+        result.append('bomb')
+        winnings = 0
+      elif num == 1:
+        result.append('777')
+        winnings = winnings + 2
+      elif num == 2:
+        result.append('cherries')
+        winnings = winnings + 4
+      elif num == 3:
+        result.append('diamonds')
+        winnings = winnings + 12
+    if 'bomb' in result:
+      print(result)
+      print("Sorry, better luck next time!")
+      print(f"You now have ${wallet} in your wallet.")
+    else:
+      print(result)
+      print(f"Congratulations! You win ${winnings}! You now have ${winnings + wallet} in your wallet.")
+    return 
+
+
+       
+                      
+slot_machine()
